9a3a0d8c1905281bb4f2115431e9541cb5c2ed47
Abhishek Kushwaha <abhishekkushwaha058@gmail.com>
2024-05-15 17:08:36 +0530

    15-05-2024

---
+++ DailyPractice/LL.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class LL {
+    class Node{
+        String data;
+        Node next;
+
+
+        Node(String data){
+
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+// add first,last
+
+public void addFirst(String data){
+
+Node newNode = new Node(data)
+    if(head==null){
+
+        head = newNode;
+        return;
+    }
+}
+
+
+
+
+
+
+public static void main(String[] args) {
+    
+LL list  = new LL();
+
+
+
+}
+
+
+}
